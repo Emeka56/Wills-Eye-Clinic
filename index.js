a69903fbdf24d11fe76786d5a5e21d76307ddf67
Emeka56 <contactemeka56@gmail.com>
2025-11-02 17:45:37 +0100

    FIX: Correcting port variable and contactus filename case

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.urlencoded({extended: true}));
